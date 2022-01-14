6f4a81d59c961f8b8dbeebf13f2e1086dbf2f04f
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2022-01-14 22:29:19 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import fetchApi from './js/fetchApi';
 import { basic } from './js/basic';
 import entryTemplate from './entry-template.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
