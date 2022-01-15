cff104563966524ca1ef3cfe310075a881a4e302
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2022-01-15 23:34:07 +0200

    add el

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ import entryTemplate from './entry-template.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+function galDisp(array) {
+  const pictGal = array.map(item => entryTemplate(item)).join('');
+  basic.gallery.insertAdjacentHTML('beforeend', pictGal);
+  galOptions();
+}
+
+basic.form.addEventListener('submit', onFormSubmit);
+let dataSearch = '';
+let page = 0;
