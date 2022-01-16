e466afb27b3e52291caf55e955fe83c6f7379cf5
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2022-01-16 16:02:28 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -15,3 +15,36 @@ function galDisp(array) {
 basic.form.addEventListener('submit', onFormSubmit);
 let dataSearch = '';
 let page = 0;
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  dataSearch = e.currentTarget.searchQuery.value;
+  page = 1;
+  if (dataSearch.trim() === '') {
+    Notify.failure('Please enter your search data.');
+    return;
+  }
+
+  fetchApi(dataSearch, page).then(response => {
+    if (response.totalHits === 0) {
+      basic.gallery.innerHTML = '';
+      Notify.failure('Sorry, there are no images matching your search query. Please try again!');
+    }
+    if (response.totalHits > 0) {
+      Notify.info(`Hooray! We found ${response.totalHits} images`);
+      basic.gallery.innerHTML = '';
+      galDisp(response.hits);
+    }
+  });
+}
+
+function galOptions() {
+  let galOptions = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionType: 'alt',
+    scrollZoomFactor: 0.5,
+    captionPosition: 'bottom',
+  });
+  galOptions.refresh();
+}
